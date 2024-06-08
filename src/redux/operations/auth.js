b278b4addfadec3a563a6b227158415ea6e4e1a2
Łukasz Wątorski <watorski.info@gmail.com>
2024-06-08 15:06:34 +0200

    .

--- src/redux/operations/auth.js
+++ src/redux/operations/auth.js
@@ -1,5 +1,7 @@
-import axios from './axiosConfig.js';
+import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
